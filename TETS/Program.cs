3570ea9421c14fb4a1db156267a2dab64d685a50
Данила Вечерский <dan_omsk@mail.ru>
2023-02-16 23:31:33 +0600

    Почистил код

---
+++ TETS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibForMindbox;
+
+namespace TETS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle(10, 10, 2);
+            var c = new Circle(-12);
+
+            IShape[] test = { t, c };
+            
+            foreach(var z in test)
+            {
+                Console.WriteLine(z.CalcArea());
+            }
+        }
+    }
+}
